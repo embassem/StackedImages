a286796895e685fae5e57560eb9ac9f8cf06a66e
Bassem Abbas <bassem.abbas@ibtikar.net.sa>
2020-03-11 18:32:20 +0200

    first commit

---
+++ StackedImage/StackedImage.h
@@ -0,0 +1,19 @@
+//
+//  StackedImage.h
+//  StackedImage
+//
+//  Created by Bassem Abbas on 3/3/20.
+//  Copyright © 2020 Bassem Abbas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StackedImage.
+FOUNDATION_EXPORT double StackedImageVersionNumber;
+
+//! Project version string for StackedImage.
+FOUNDATION_EXPORT const unsigned char StackedImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackedImage/PublicHeader.h>
+
+
